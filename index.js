b039721e32b51ec6ceb9a967adf2d4d949b857b1
atul-coderial <ac411848@gmail.com>
2021-05-17 13:19:19 +0530

    we have added views with ejs

---
+++ index.js
@@ -0,0 +1,25 @@
+//Load library of express
+const express = require('express');
+//Assign the port no.
+const port = 8000;
+//calling the express functions in app 
+const app = express();
+
+//set views to render the home page in the browser
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+//Responsing the home page to the browser
+app.get('/', function(req, res){
+    return res.render('home', {
+        title: "To-Do List",
+    })
+})
+
+//listing the server
+app.listen(port, function(err){
+    if(err){
+        console.log('Error is occured at running the server', err);
+    }
+    console.log('Server is successfully running on port:', port);
+})
